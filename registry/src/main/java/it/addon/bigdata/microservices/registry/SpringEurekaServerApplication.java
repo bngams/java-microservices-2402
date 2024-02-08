f5099cbd44c47959c472c894202faf793f5d93ba
bngams <n.boris@devidia.net>
2024-02-08 01:23:05 +0100

    create projects

---
+++ registry/src/main/java/it/addon/bigdata/microservices/registry/SpringEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package it.addon.bigdata.microservices.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer	// Enable eureka server
+public class SpringEurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEurekaServerApplication.class, args);
+    }
+}
